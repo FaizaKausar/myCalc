584949fff9ba7ae3bb742041341ab9403afd5f23
rahman100pk <rahman100pk@gmail.com>
2023-05-12 19:30:26 +0500

    Initial commit

---
+++ node_modules/faizakausar.666calc/add.js
@@ -0,0 +1,34 @@
+/*export {};
+const add = (a: number, b: number) => {
+
+    let sum = a+b
+    return sum
+}
+
+export default add;
+console.log(add); */
+/*let min = 5;
+let max = 8;
+let sum = 0;
+
+for(let i = min; i <= max; i++){
+  let remainder = i % 2;
+  console.log(`The remainder of ${i} divided by 2 is ${remainder}.`);
+  if(remainder === 0) {
+    console.log(`${i} is an even number.`);
+  }
+}*/
+``;
+//for (let i = 1; i <= 10; i++) {
+//console.log(`5 x ${i} = ${5 * i}`);
+// }
+let min = 0;
+let count = 1;
+let max = 100;
+let nums = 60;
+for (count; count <= nums; count++) {
+    if (count % 2 !== 0) {
+        console.log(count);
+    }
+}
+export {};
